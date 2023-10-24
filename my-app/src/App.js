de2f59fefb3ee8ac249d0d0cac2131b3398ffcb1
Gabriella.E <63784697+bubleygum@users.noreply.github.com>
2023-10-24 12:45:26 +0700

    ,

---
+++ my-app/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="arjs-loader">
+        <div>Loading, please wait...</div>
+      </div>
+      <a-scene
+        vr-mode-ui="enabled: false;"
+        renderer="logarithmicDepthBuffer: true;"
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
+      >
+
+        <a-nft
+          type="nft"
+          url="public/owl"
+          smooth="true"
+          smoothCount="10"
+          smoothTolerance=".01"
+          smoothThreshold="5"
+        >
+          <a-entity
+            gltf-model="public/test.gltf"
+            scale="5 5 5"
+            position="50 150 0"
+          >
+          </a-entity>
+        </a-nft>
+        <a-entity camera></a-entity>
+      </a-scene>
+    </div>
+  );
+}
+
+export default App;

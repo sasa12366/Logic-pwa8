5cfdd298ef9eabd0d7be39b0830058a941fdb4c4
Gabriella.E <63784697+bubleygum@users.noreply.github.com>
2023-10-26 18:53:23 +0700

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <div className="arjs-loader">
+      {/* 3d gltf */}
+      {/* <div className="arjs-loader">
         <div>Loading, please wait...</div>
       </div>
       <a-scene
@@ -23,15 +23,49 @@ function App() {
           smoothThreshold="5"
         >
           <a-entity
-            gltf-model="./owl/test.gltf"
+            gltf-model="./test.gltf"
             scale="5 5 5"
             position="50 150 0"
           >
           </a-entity>
         </a-nft>
         <a-entity camera></a-entity>
+      </a-scene> */}
+
+
+      {/* mp4 */}
+      <div className="arjs-loader">
+        <div>Loading, please wait...</div>
+      </div>
+      <a-scene
+        vr-mode-ui="enabled: false;"
+        renderer='antialias: true; alpha: true; precision: medium;'
+        embedded arjs='trackingMethod: best; sourceType: webcam; debugUIEnabled: false;'>
+
+        <a-assets>
+        <video src="vid.mp4" preload="auto" id="vid" loop webkit-playsinline="true" autoPlay muted playsInline></video>
+
+        </a-assets>
+
+        <a-nft
+          videohandler
+          type='nft' url='./owl/owlCard/1'
+          smooth="true" smoothCount="10" smoothTolerance="0.01" smoothThreshold="5"
+        >
+          <a-video
+            src="#vid"
+            position='50 150 -100'
+            rotation='90 0 180'
+            width='300'
+            height='175'
+          >
+          </a-video>
+        </a-nft>
+        <a-entity camera></a-entity>
       </a-scene>
     </div>
+
+
   );
 }
 
